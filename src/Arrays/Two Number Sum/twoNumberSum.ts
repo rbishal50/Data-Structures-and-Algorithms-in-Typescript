a8335d4ba8e032f0db6dfefcd23d0c2ce95da8ef
Bishal Rana <rbishal50@gmail.com>
2023-07-27 10:28:10 +0545

    Update comments

--- src/Arrays/Two Number Sum/twoNumberSum.ts
+++ src/Arrays/Two Number Sum/twoNumberSum.ts
@@ -2,15 +2,21 @@
 // O(N) time for going through the array (Worst case)
 
 export function twoNumberSum(array: number[], targetSum: number) {
+  // Create a set for storing seen items
   const set: Set<Number> = new Set();
 
+  // Loop through the array
   for (const num of array) {
+    // Check if another number is in the set
     const potentialMatch = targetSum - num;
 
+    // If it exists return the items
     if (set.has(potentialMatch)) return [num, potentialMatch];
 
+    // Else add the number to the set
     set.add(num);
   }
 
+  // Return [] if no items found
   return [];
 }
